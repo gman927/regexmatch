1535bd8354e9e5626bbf64977f58a58d6b00c8c9
gman927 <jaemy927@gmail.com>
2016-09-15 08:21:07 +0800

    detects the word false

--- regexmatch/src/regexmatch/Regexmatch.java
+++ regexmatch/src/regexmatch/Regexmatch.java
@@ -6,12 +6,15 @@ public class Regexmatch {
     public static boolean isTrue(String str){
         return str.matches("[tT]rue");
     }
-    /**
-     * @param args the command line arguments
-     */
+    
+    public static boolean containsFalse(String str){
+        return str.matches(".*false.*");
+    }
+    
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println(isTrue("true"));
+        System.out.println(containsFalse("asdlkj"));
     }
     
 }
