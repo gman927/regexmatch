a2df289148fe3a466987e8bebbb042a13a1d0927
gman927 <jaemy927@gmail.com>
2016-09-15 08:14:18 +0800

    Adds first regex

--- regexmatch/src/regexmatch/Regexmatch.java
+++ regexmatch/src/regexmatch/Regexmatch.java
@@ -2,12 +2,16 @@
 package regexmatch;
 
 public class Regexmatch {
-
+    
+    public static boolean isTrue(String str){
+        return str.matches("[tT]rue");
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println(isTrue("true"));
     }
     
 }
